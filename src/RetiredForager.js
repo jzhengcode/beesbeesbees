2b96c2474f09ca472efeafbb2c3ead4ad5379a05
Janet Zheng <janet.zhengg@gmail.com>
2020-05-18 15:23:05 -0700

    all tests passed

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -18,19 +18,47 @@ var RetiredForagerBee = function() {
   // create an instance of the ForegerBee subclass
   // alter the value in the age property to 40
   // alter job property to "gamble"
+  // alter the color property to "grey"
   // alter canFy property to false
+  // falt
   // set treasureChest property to an empty array
+
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.color = "grey";
+  this.canFly = false;
+  this.treasureChest = [];
 };
 
 //create a prototypal method will direct failed look ups to Grub prototype
 // add a prototypal constructor property will report ForagerBee
-// add a forage method
+
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+// alter the forage method
+  // I - none
+  // O - will output "I am too old, let me play cards instead"
+  // C - none
+  // E - none
+
+  // Explanation: tje function will return a string "I am too old, let me play cards instead"
+RetiredForagerBee.prototype.forage = function(){
+  return "I am too old, let me play cards instead"
+}
+
+// add a gamble method
   // I - treasure
   // O - none. side effect treasureChest is altered
   // C - none
   // E - none
 
   //Explanation: Will add the input value treasure to the array property treasureChest
+
+  RetiredForagerBee.prototype.gamble = function(treasure){
+    this.treasureChest.push(treasure);
+  }
 
 
 
