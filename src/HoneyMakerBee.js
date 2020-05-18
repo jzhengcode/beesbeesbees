22d56c76aa8b37d2ea8a51c0076a16825345099a
Janet Zheng <janet.zhengg@gmail.com>
2020-05-18 13:26:10 -0700

    pseudo code added

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,33 @@
+/*
+ I - nothing
+ O - an object with various properties and methods inhereited from the Bee and Grub subclasses, as well as their own properties and methods.
+ E - none
+ */
+
+ /*  Explanation: Have to write a function that doesn't take in any inputs but will create on object that:
+  has it's own uniq age
+  a unique job property
+  inherits the color property from Bee
+  inherits the food property grub
+  a unique job property
+  a honey pot property
+*/
+
+
 var HoneyMakerBee = function() {
+  // create an instance of the Bee subclass
+  // alter the value in the age property to 10
+  // alter job property to "make honey"
+  // create honeyPot property
+
 };
+
+//create a prototypal method eat is an instance of the Grub prototypal object
+// add a prototypal constructor property will report ForagerBee
+// add a makeHoney method
+  // I - some number value
+  // O - none. side effect honeyPot is altered
+  // C - input as to be a postive number
+  // E - none
+
 
