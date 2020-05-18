811010600e68c2a4debf50338b8e2af11122f53c
Janet Zheng <janet.zhengg@gmail.com>
2020-05-18 12:55:54 -0700

    some code added. have not tested

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -20,14 +20,26 @@ var ForagerBee = function() {
   // alter the value in the age property to 10
   // alter job property to "find pollen"
   // create canFly property and set to true
-  // create treasureChear property and set to empty array
+  // create treasureChest property and set to empty array
+
+  Bee.call(this);
+  this.age = 10;
+  this.job = "find pollen";
+  this.canFly = true;
+  this.treasureChest = [];
 
 }
 
-//create a prototypal method eat is an instance of the Bee prototypal object
+//create a prototypal method eat is an instance of the Grub prototypal object
 // add a prototypal constructor property will report ForagerBee
 // add a forage method with input of treausre
-  // push treasure onto treasureCheat array
+  // push treasure onto treasureChest array
+
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+}
 
 
 
