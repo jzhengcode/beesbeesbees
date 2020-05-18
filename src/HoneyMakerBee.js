1eb2d30be88f8665b3f69fa02d4fd5b958c8190d
Janet Zheng <janet.zhengg@gmail.com>
2020-05-18 13:35:31 -0700

    HoneyBee and ForagerBee tests passed

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -19,15 +19,36 @@ var HoneyMakerBee = function() {
   // alter the value in the age property to 10
   // alter job property to "make honey"
   // create honeyPot property
-
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
 
 //create a prototypal method eat is an instance of the Grub prototypal object
 // add a prototypal constructor property will report ForagerBee
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
 // add a makeHoney method
-  // I - some number value
+  // I - none
   // O - none. side effect honeyPot is altered
-  // C - input as to be a postive number
+  // C - none
   // E - none
 
+  //Explanation: Will add 1 ti the honey pot property
 
+HoneyMakerBee.prototype.makeHoney = function (){
+  this.honeyPot += 1;
+}
+// add give honey method
+// I - none
+  // O - none. side effect honeyPot is altered
+  // C - none
+  // E - none
+
+  //Explanation: Will subtract 1 from the honey pot property
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot -= 1;
+}
