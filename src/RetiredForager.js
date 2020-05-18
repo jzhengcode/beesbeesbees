7f0ffe110e8640c1c31518c777d6efb9103ee75d
Janet Zheng <janet.zhengg@gmail.com>
2020-05-18 14:46:36 -0700

    pseudocode added

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,36 @@
 var RetiredForagerBee = function() {
+  /*
+ I - nothing
+ O - an object with various properties and methods inhereited from the Bee, Grub, and Forager subclasses, as well as their own properties and methods.
+ E - none
+ */
+
+ /*  Explanation: Have to write a function that doesn't take in any inputs but will create on object that:
+  has it's own uniq age
+  an unique job property
+  an unique
+  inherits the color property from Bee
+  inherits the food property grub
+  a unique job property
+  a honey pot property
+*/
+
+  // create an instance of the ForegerBee subclass
+  // alter the value in the age property to 40
+  // alter job property to "gamble"
+  // alter canFy property to false
+  // set treasureChest property to an empty array
 };
+
+//create a prototypal method will direct failed look ups to Grub prototype
+// add a prototypal constructor property will report ForagerBee
+// add a forage method
+  // I - treasure
+  // O - none. side effect treasureChest is altered
+  // C - none
+  // E - none
+
+  //Explanation: Will add the input value treasure to the array property treasureChest
+
+
 
